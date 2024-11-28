4e6381f94f453e8eaba4e5aad5b12ace7202c62b
223028842 <86828984+223028842@users.noreply.github.com>
2024-11-28 23:02:54 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ from utilities.test_common_utilities import common_utilities
 @pytest.fixture(scope="session")
 #def setup(browser):
 def context_creation(playwright):
-    browser = playwright.chromium.launch(headless=False,args=["--start-maximized"])
+    browser = playwright.chromium.launch(headless=True,args=["--start-maximized"])
     context = browser.new_context(ignore_https_errors=True,no_viewport=True)
     page=context.new_page()
 
@@ -21,7 +21,7 @@ def context_creation(playwright):
 
 @pytest.fixture()
 def login_set_up(context_creation,playwright):
-    browser = playwright.chromium.launch(headless=False,args=["--start-maximized"])
+    browser = playwright.chromium.launch(headless=True,args=["--start-maximized"])
     context = browser.new_context(storage_state='auth_state.json',ignore_https_errors=True,no_viewport=True)
     page= context.new_page()
 
